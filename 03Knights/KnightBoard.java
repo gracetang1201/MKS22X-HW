e4370769f9efcce5fc870d00345bc69532ba7b50
gracetang1201 <gracetang1201@gmail.com>
2017-02-14 12:33:00 -0500

    added 03

---
+++ 03Knights/KnightBoard.java
@@ -0,0 +1,16 @@
+public class KnightBoard{
+    int[][]board;
+    public KnightBoard(int startingRows,int startingCols){
+    }
+
+    public String toString(){
+    }
+
+    public void solve(){
+    }
+
+    private boolean solveH(int r, int c, int i){
+    }
+
+}
+			
