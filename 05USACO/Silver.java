d621b4d624fb7f422830a07db3f54cc66776cad6
gracetang1201 <gracetang1201@gmail.com>
2017-03-13 19:29:53 -0400

    finished!

--- 05USACO/Silver.java
+++ 05USACO/Silver.java
@@ -8,9 +8,11 @@ public class Silver{
     public static void main(String[]args){
 	Silver s = new Silver();
 	s.importFile("file1.txt");
-	System.out.println(s);
+	//System.out.println(s);
 	System.out.println(s.solve());
-	System.out.println(s);
+	//System.out.println(s.solve());
+	//System.out.println(s.move(0, 0));
+	//System.out.println(s);
     }
     
     public void importFile(String filename){
@@ -18,8 +20,8 @@ public class Silver{
 	Scanner s = null;
 	//Scanner c = null;
 	try{
-	   s = new Scanner(f);
-	   //c = new Scanner(f);
+	    s = new Scanner(f);
+	    //c = new Scanner(f);
 	}catch (FileNotFoundException e){
 	    System.out.println("file not found");
 	}
@@ -58,70 +60,43 @@ public class Silver{
 	
     }
 
-    public int move(int row, int col){
-	int count = land[row][col];
-	//land[row][col] = 0;
-	if(!(land[row][col] == -1)){
-	    if(land[row][col] == 0){
-		if(row - 1 >= 0 && !(land[row-1][col]== -1)){
-		    count += land[row-1][col];
-		    //land[row-1][col] += land[row][col];
+   public int move(int r, int c){
+	int total = 0;
+	//land1[r][c] = land[r+1][c] + land[r-1][c] + land[r][c+1] + land[r][c-l];
+	if(r-1 >= 0 && land[r-1][c] != -1){
+	    total += land[r-1][c];
+	}
+	if(r+1 < land.length && land[r+1][c] != -1){
+	    total += land[r+1][c];
+	}
+	if(c-1 >= 0 && land[r][c-1] != -1){
+	    total += land[r][c-1];
+	}
+	if(c+1 < land[0].length && land[r][c+1] != -1){
+	    total += land[r][c+1];
+	}
+	return total; 
+    }
+    public int solve(){
+	for (int steps = 0; steps < seconds; steps++){
+	    for(int r = 0; r < land.length; r++){
+		for(int c = 0; c < land[r].length; c++){
+		    if(land[r][c] != -1){
+			land1[r][c] = move(r, c);
+		    }else{
+			land1[r][c] = -1;
+		    }
 		}
-		if (row + 1 < land.length && !(land[row+1][col] == -1)){
-		    count += land[row+1][col];
-		    //land[row+1][col] += land[row][col];
+	    }
+	    for(int r = 0; r < land.length; r++){
+		for(int c = 0; c < land[r].length; c++){
+		    land[r][c] = land1[r][c];
 		}
-		if (col - 1 >= 0 && !(land[row][col-1] == -1)){
-		    count += land[row][col-1];
-		    //land[row][col-1] += land[row][col-1];
-		}
-		if(col+1<land[0].length && !(land[row][col+1] == -1)){
-		    count += land[row][col+1];
-		    //land[row][col+1] += land[row][col+1];
-		}
-		
-	    }else{
-		land1[row][col] = 0;
 	    }
 	    
 	}
-	return count;
+	return land1[endx][endy];
     }
-
-
-
-    public int solve(){
-	int time = 1;
-	//for(int time = 1; seconds==time; time++){
-	    while(time<=seconds){
-	    for(int r = 0; r < land.length; r++){
-		for(int c = 0; c < land[0].length; c++){
-		    if(!(land[r][c] == 0)){
-			land1[r][c] = move(r, c);
-		    }else{
-			    
-			land1[r][c] = -1;
-		    }
-		    if(!(land[r][c] == -1)){
-			if(land[r][c] == 0){
-			    land1[r][c] = move(r, c);
-			}else{
-			    land1[r][c] = 0;
-			}
-		    }else{
-			land1[r][c] = -1;
-		    }
-		    //land1[row][col] = 0;
-		}
-	    }
-	    time++;
-	}
-	//return land1[endx][endy];  
-	
-	return land1[endx][endy];  
-	
-    }
-    //return land1[endx][endy];
     
 
     public String toString(){
@@ -139,18 +114,18 @@ public class Silver{
 	    }
 	}
 	/*
-	for(int r = 0; r < land1.length; r++){
-	    for(int c = 0; c < land1[r].length; c++){
+	  for(int r = 0; r < land1.length; r++){
+	  for(int c = 0; c < land1[r].length; c++){
 	
-		if(c == land1[r].length-1){
+	  if(c == land1[r].length-1){
 		   
-		    ans = ans + " " + land1[r][c] + "\n";
-		}else{
-		    ans = ans + " " + land1[r][c];
-		}
+	  ans = ans + " " + land1[r][c] + "\n";
+	  }else{
+	  ans = ans + " " + land1[r][c];
+	  }
 	    
-	    }
-	}
+	  }
+	  }
 	*/
 	return ans;
     }
