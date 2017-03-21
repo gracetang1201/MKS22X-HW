695c5914a35286fe84b07088a24488fcfaeae081
gracetang1201 <gracetang1201@gmail.com>
2017-03-20 20:31:52 -0400

    fixed up sqrt

--- 01Recursion/Recursion.java
+++ 01Recursion/Recursion.java
@@ -1,11 +1,14 @@
 public class Recursion{
     public static void main(String[]args){
-	System.out.println(sqrt(-121.0));
+	System.out.println(sqrt(0.0));
     }
     public static String name(){
 	return "Tang,Grace";
     }
     public static boolean closeEnough(double a, double b){
+	if (b < 0){
+	    throw new IllegalArgumentException("you can't squareroot a negative");
+	}
 	if (Math.abs(a - b)/a <= 0.00000000001){
 	    return true;
 	}else{
@@ -13,15 +16,19 @@ public class Recursion{
 	}
     }
     public static double sqrt(double n){
+	if(n==0.0){
+	    return 0.0;
+	}
 	if(n<0){
 	    throw new IllegalArgumentException("you can't squareroot a negative");
 	}
-	if(closeEnough(0, n)){
-	    return 0.0;
-	}
+	
 	return squareroot(n, n/2);
     }
     public static double squareroot(double n, double guess){
+	if(n<0){
+	    throw new IllegalArgumentException("you can't squareroot a negative");
+	}
 	if(closeEnough(n, guess*guess)){
 	    return guess;
 	}else{
