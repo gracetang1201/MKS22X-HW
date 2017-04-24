0e0ad33d83bbc97242a11e1b459109ed61207b8d
gracetang1201 <gracetang1201@gmail.com>
2017-04-24 18:43:57 -0400

    modified names and stuff

---
+++ 11/StackCalc.java
@@ -0,0 +1,61 @@
+import java.util.Stack;
+public class StackCalc{
+    public static void main(String[] args)
+{
+    System.out.println(StackCalc.eval("10 2 +"));//12.0
+    System.out.println(StackCalc.eval("10 2 -"));//8.0
+    System.out.println(StackCalc.eval("10 2.0 +"));//12.0
+    System.out.println(StackCalc.eval("11 3 - 4 + 2.5 *"));//30.0
+    System.out.println(StackCalc.eval("8 2 + 99 9 - * 2 + 9 -"));//839.0
+    System.out.println(StackCalc.eval("10 2 + 10 * 1 + 1 1 1 + + + 10 10 + -"));//104.0
+}
+    public static boolean isOp(String s){
+	String operators = "+/-*";
+	return(operators.indexOf(s) > -1);
+    }
+    public static double apply(String op, String e, String f){
+	/*	if (op.equals("+")){
+		return a + b
+		}
+		if (op.equals("*")){
+		return a * b
+		}
+		if (op.equals("/")){
+		return a / b
+		}
+		if (op.equals("-")){
+		return a - b
+		}
+	*/
+	
+	double a = Double.parseDouble(f);
+	double b = Double.parseDouble(e);
+	double answer = 0.0;
+	switch ("/-*+".indexOf(op)){
+	case 0: answer = a / b;
+	    break;
+	case 1: answer = a - b;
+	    break;
+	case 2: answer = a * b;
+	    break;
+	case 3: answer = a + b;
+	    break;
+	    
+	}
+	return answer;
+    }
+    public static double eval(String s){
+	String[]tokens = s.split(" ");
+	Stack nums = new Stack();
+	for(String token: tokens){
+	    if(isOp(token)){
+		nums.push(Double.toString(apply(token, (String)nums.pop(), (String)nums.pop())));
+	    }else{
+		nums.push(token);
+	    }
+	}
+	return Double.parseDouble((String)nums.pop());
+	
+    }
+
+}
