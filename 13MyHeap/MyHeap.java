f7e6beb4fc68aa6d5da110f0d7da69b6fcee32b8
gracetang1201 <gracetang1201@gmail.com>
2017-05-07 10:39:14 -0400

    done!

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,24 +1,32 @@
 public class MyHeap{
     public String[] heap = new String[10];
     int lastindex; //THIS IS THE INDEX YOU ARE ADDING TO/ THE FIRST OPEN INDEX
+    boolean isMax;
     
     public static void main(String[]args){
-	MHeap m = new MHeap();
+	MyHeap m = new MyHeap();
+	MyHeap b = new MyHeap(false);
 	m.add("A");
 	m.add("B");
 	m.add("C");
 	m.add("D");
 	m.add("E");
 	System.out.println(m);
-	//m.add("F");
-	m.remove();
-	System.out.println(m);
-	m.remove();
-	System.out.println(m);
+	b.add("A");
+	b.add("B");
+	b.add("C");
+	b.add("D");
+	b.add("E");
+	System.out.println(b);
 	
 	
     }
     public MyHeap(){
+	lastindex = 1;
+	isMax = true;
+    }
+    public MyHeap(boolean b){
+	isMax = b;
 	lastindex = 1;
     }
     public String toString(){
@@ -28,6 +36,9 @@ public class MyHeap{
 	}
 	return ans;
     }
+    public String peek(){
+	return heap[1];
+    }
     public void add(String newstr){
 	if(lastindex == heap.length){
 	    grow();
@@ -36,65 +47,95 @@ public class MyHeap{
 	heap[lastindex] = newstr;
 	
 	int temp = lastindex;
-	while(temp != 1){
-	    //System.out.println(temp);
-	    if(heap[temp].compareTo(heap[temp/2]) >= 1){
-		//System.out.println(heap[temp]);
-		//System.out.println(heap[temp/2] + "------");
-		swap(temp, temp/2);
-	    }else{
-		System.out.println("Did not swap" + temp);
+	if(isMax){
+	    while(temp != 1){
+		//System.out.println(temp);
+		if(heap[temp].compareTo(heap[temp/2]) >= 1){
+		    //System.out.println(heap[temp]);
+		    //System.out.println(heap[temp/2] + "------");
+		    swap(temp, temp/2);
+		}else{
+		    System.out.println("Did not swap" + temp);
+		}
+		temp = temp/2;
 	    }
-	    temp = temp/2;
+	}else{
+	    while(temp != 1){
+		//System.out.println(temp);
+		if(heap[temp].compareTo(heap[temp/2]) <= -1){
+		    //System.out.println(heap[temp]);
+		    //System.out.println(heap[temp/2] + "------");
+		    swap(temp, temp/2);
+		}else{
+		    System.out.println("Did not swap" + temp);
+		}
+		temp = temp/2;
+	    }
 	}
+
 	lastindex++;
     }
-    public void remove(){
+    public String remove(){
 	/*
-	heap[1] = heap[lastindex-1];
-	heap[lastindex-1] = null;
-	int temp = 1;
+	  heap[1] = heap[lastindex-1];
+	  heap[lastindex-1] = null;
+	  int temp = 1;
 	
-	while(temp < lastindex / 2){
-	    int s = heap[temp*2].compareTo(heap[temp*2+1]);
-	    if(s >= 1){
-		if(heap[temp].compareTo(heap[temp*2])<=-1){
-		    swap(temp, temp*2);
-		}
-		temp = temp *2;
-	    }else if(s <= -1){
-		System.out.println(temp);
-		System.out.println(temp*2+1);
-		if(heap[temp].compareTo(heap[temp*2+1])<=-1){
-		    swap(temp, temp*2+1);
-		}
-		temp = temp*2+1;
-	    }
-	}
+	  while(temp < lastindex / 2){
+	  int s = heap[temp*2].compareTo(heap[temp*2+1]);
+	  if(s >= 1){
+	  if(heap[temp].compareTo(heap[temp*2])<=-1){
+	  swap(temp, temp*2);
+	  }
+	  temp = temp *2;
+	  }else if(s <= -1){
+	  System.out.println(temp);
+	  System.out.println(temp*2+1);
+	  if(heap[temp].compareTo(heap[temp*2+1])<=-1){
+	  swap(temp, temp*2+1);
+	  }
+	  temp = temp*2+1;
+	  }
+	  }
 	*/
+	String ans = heap[1];
 	heap[1] = heap[lastindex-1];
+	
 	heap[lastindex-1] = null;
 	int temp = 1;
 	lastindex--;
-	while(temp < lastindex / 2){
-	    int s = heap[temp*2].compareTo(heap[temp*2+1]);
-	    if(s >= 1){
-		if(heap[temp].compareTo(heap[temp*2])<=-1){
-		    swap(temp, temp*2);
-		}
-	    }else if(s <= -1){
+	if(!isMax){
+	    while(temp < lastindex / 2){
+		int s = heap[temp*2].compareTo(heap[temp*2+1]);
+		if(s >= 1){
+		    if(heap[temp].compareTo(heap[temp*2+1])<=-1){
+			swap(temp, temp*2+1);
+		    }
+		}else if(s <= -1){
 	    
-		if(heap[temp].compareTo(heap[temp*2+1])<=-1){
-		    swap(temp, temp*2+1);
+		    if(heap[temp].compareTo(heap[temp*2])<=-1){
+			swap(temp, temp*2);
+		    }
 		}
+		temp = temp*2;
 	    }
-	    temp = temp*2;
+	}else{
+	    while(temp < lastindex / 2){
+		int s = heap[temp*2].compareTo(heap[temp*2+1]);
+		if(s >= 1){
+		    if(heap[temp].compareTo(heap[temp*2])>=-1){
+			swap(temp, temp*2);
+		    }
+		}else if(s <= -1){
+	    
+		    if(heap[temp].compareTo(heap[temp*2+1])<=-1){
+			swap(temp, temp*2+1);
+		    }
+		}
+		temp = temp*2;
+	    }
 	}
-
-	
-	
-    
-	
+	return ans;
     }
     public void swap(int in1, int in2){
 	String temp = heap[in1];
