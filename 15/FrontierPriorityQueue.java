4c417b85ee8a9d49c9f754853bda53af02d84709
gracetang1201 <gracetang1201@gmail.com>
2017-05-30 18:08:02 -0400

    changed stuff bc people said they looked at my code so now im paranoid that it might look like i cheated or smth

--- 15/FrontierPriorityQueue.java
+++ 15/FrontierPriorityQueue.java
@@ -2,96 +2,137 @@ import java.util.*;
 
 public class FrontierPriorityQueue implements Frontier{
 
-    private ArrayList<Location> data;
-
+    public Location[] heap = new Location[10];
+    int lastindex; //THIS IS THE INDEX YOU ARE ADDING TO/ THE FIRST OPEN INDEX
+    boolean isMax;
+    
+   
     public FrontierPriorityQueue(){
-
-	data = new ArrayList<Location>();
-	data.add(0,null);
+	lastindex = 1;
+	isMax = true;
     }
-
-    public int getSize(){
-	return data.size();
+    public FrontierPriorityQueue(boolean b){
+	isMax = b;
+	lastindex = 1;
     }
-
-    public void add(Location x){
-	data.add(x);
-	pushUp();
-    }
-
-    public Location next(){ //remove?
-	Location temp = data.get(1);
-	data.set(1, data.get(data.size() - 1));
-	data.remove(data.size() - 1);
-	pushDown();
-	return temp;
-    }
-
     public Location peek(){
-	return data.get(1);
+	return heap[1];
     }
-
-    private void pushUp(){
-	Location temp = data.get(data.size() - 1);
-	int index = data.size() - 1;
-
-	    while((index/2 > 0) &&
-		  (temp.compareTo(data.get(index/2)) < 1)){
-		swap(data, index, index/2);
-		index = index/2;
-	    }
-    }
-	
-
-    private void pushDown(){
-
-	if(data.size() == 1){
-	    return;
+    public void add(Location newstr){
+	if(lastindex == heap.length){
+	    grow();
 	}
+	heap[lastindex] = newstr;
 	
-	Location temp = data.get(1);
-	int index = 1;
-
-	while(
-	      (((index*2) + 1 < data.size()) &&
-	       ((temp.compareTo(data.get(index*2)) > -1) ||
-		(temp.compareTo(data.get((index*2)+1)) > -1)))
-	      ||
-	      ((index*2 < data.size()) &&
-	       ((temp.compareTo(data.get(index*2)) > -1)))
-	      ){
-		
-	    if((temp.compareTo(data.get(index*2)) > -1)&&
-	       ((index*2) + 1 >= data.size() ||
-		(data.get(index*2).compareTo(data.get((index*2) +1)) < 1)
-		)){
-		swap(data, index, index*2);
-		index = index*2;
+	int temp = lastindex;
+	if(isMax){
+	    while(temp != 1){
+		//System.out.println(temp);
+		if(heap[temp].compareTo(heap[temp/2]) >= 1){
+		    swap(temp, temp/2);
+		}
+		temp = temp/2;
 	    }
-	    else{
-		swap(data, index, (index*2)+1);
-		index = (index*2)+1;
+	}else{
+	    while(temp != 1){
+		//System.out.println(temp);
+		if(heap[temp].compareTo(heap[temp/2]) <= -1){
+		    swap(temp, temp/2);
+		}
+		temp = temp/2;
 	    }
 	}
+
+	lastindex++;
     }
+    public int getSize(){
+	return lastindex;
+    }
+    public Location next(){
+	/*
+	  heap[1] = heap[lastindex-1];
+	  heap[lastindex-1] = null;
+	  int temp = 1;
+	
+	  while(temp < lastindex / 2){
+	  int s = heap[temp*2].compareTo(heap[temp*2+1]);
+	  if(s >= 1){
+	  if(heap[temp].compareTo(heap[temp*2])<=-1){
+	  swap(temp, temp*2);
+	  }
+	  temp = temp *2;
+	  }else if(s <= -1){
+	  System.out.println(temp);
+	  System.out.println(temp*2+1);
+	  if(heap[temp].compareTo(heap[temp*2+1])<=-1){
+	  swap(temp, temp*2+1);
+	  }
+	  temp = temp*2+1;
+	  }
+	  }
+	*/
+	Location ans = heap[1];
+	heap[1] = heap[lastindex-1];
+	
+	heap[lastindex-1] = null;
+	int temp = 1;
+	lastindex--;
+	//	System.out.println("this is the last index" + lastindex);
+	/*
+	  if(heap.length == 2 && !isMax && heap[1] > heap[2]){
+	  swap(1, 2);
+	  }
+	*/
+	
+	if(isMax){
+	    while(temp < lastindex / 2){
+		//int s = heap[temp*2].compareTo(heap[temp*2+1]);
+		//if(s <= -1){
+		if(heap[temp].compareTo(heap[temp*2])<=-1){
+		    swap(temp, temp*2+1);
+		    //}
+		}else{ //if(s >= 1){
 	    
-
-
-    private void swap(ArrayList<Location> ary, int ind, int ind2){
-	Location temp = ary.get(ind);
-	ary.set(ind, ary.get(ind2));
-	ary.set(ind2, temp);
-    }
-
-    public String toString(){
-	String temp = "";
-
-	for(int i = 1; i < data.size(); i++){
-	    temp += data.get(i).getRow() + " ";
+		    if(heap[temp].compareTo(heap[temp*2+1])<=-1){
+			swap(temp, temp*2);
+		    }
+		}
+		temp = temp*2;
+	    }
+	}else{
+	    while(temp < lastindex / 2){
+		//int s = heap[temp*2].compareTo(heap[temp*2+1]);
+		if(heap[temp*2].compareTo(heap[temp*2+1]) >= 1){
+	    
+		    if(heap[temp].compareTo(heap[temp*2+1])>=-1){
+			swap(temp, temp*2+1);
+			//temp = temp * 2 + 1;
+		    }
+		}else{
+		    if(heap[temp].compareTo(heap[temp*2])>=-1){
+			swap(temp, temp*2);
+			//temp *= 2;
+		    }
+		}
+		temp = temp*2;
+		
+	    }
 	}
-       
-
-	return temp;
+	return ans;
+	
     }
+    public void swap(int  in1, int in2){
+	Location temp = heap[in1];
+	heap[in1] = heap[in2];
+	heap[in2] = temp;
+    }
+    public void grow(){
+        Location[]temp = new Location[heap.length*2];
+	for(int i = 0; i < heap.length; i++){
+	    temp[i] = heap[i];
+	}
+	heap = temp;
+    }
+
 
 }
